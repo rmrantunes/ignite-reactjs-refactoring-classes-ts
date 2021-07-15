8604ccb4ba132482883685bfaf32e9c71808c550
Rafael Antunes <rafaelmessias710@gmail.com>
2021-07-15 11:31:11 -0400

    refactor: migrate Modal to FC

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,67 +1,47 @@
-import { Component } from 'react';
-import ReactModal from 'react-modal';
+import { useState, useEffect } from "react";
+import ReactModal from "react-modal";
 
 type ModalProps = {
-  isOpen: boolean
-  setIsOpen(): void
-}
-
-type ModalState = {
-  modalStatus: boolean
-}
-
-class Modal extends Component<ModalProps,ModalState> {
-  constructor(props: ModalProps) {
-    super(props);
-
-    const { isOpen } = this.props;
-    this.state = {
-      modalStatus: isOpen
-    }
-  }
-
-  componentDidUpdate(prevProps: ModalProps) {
-    const { isOpen } = this.props;
-
-    if (prevProps.isOpen !== isOpen) {
-      console.log(this.props)
-      this.setState({ modalStatus: isOpen })
-    }
-  }
-
-  render() {
-    const { children, setIsOpen } = this.props;
-    const { modalStatus } = this.state;
-
-    return (
-      <ReactModal
-        shouldCloseOnOverlayClick={!false}
-        onRequestClose={setIsOpen}
-        isOpen={modalStatus}
-        ariaHideApp={false}
-        style={{
-          content: {
-            top: '50%',
-            left: '50%',
-            right: 'auto',
-            bottom: 'auto',
-            marginRight: '-50%',
-            transform: 'translate(-50%, -50%)',
-            background: '#F0F0F5',
-            color: '#000000',
-            borderRadius: '8px',
-            width: '736px',
-            border: 'none',
-          },
-          overlay: {
-            backgroundColor: '#121214e6',
-          },
-        }}
-      >
-        {children}
-      </ReactModal>
-    );
-  }
+  isOpen: boolean;
+  setIsOpen(): void;
+  children: React.ReactNode;
 };
+
+function Modal(props: ModalProps) {
+  const [modalStatus, setModalStatus] = useState(props.isOpen);
+
+  useEffect(() => {
+    setModalStatus(props.isOpen);
+  }, [props.isOpen]);
+
+  return (
+    <ReactModal
+      shouldCloseOnOverlayClick={!false}
+      onRequestClose={props.setIsOpen}
+      isOpen={modalStatus}
+      ariaHideApp={false}
+      style={{
+        content: {
+          top: "50%",
+          left: "50%",
+          right: "auto",
+          bottom: "auto",
+          marginRight: "-50%",
+          transform: "translate(-50%, -50%)",
+          background: "#F0F0F5",
+          color: "#000000",
+          borderRadius: "8px",
+          width: "736px",
+          border: "none",
+        },
+        overlay: {
+          backgroundColor: "#121214e6",
+        },
+      }}
+    >
+      {props.children}
+    </ReactModal>
+  );
+}
 
 export default Modal;
